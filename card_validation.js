9323afa426fc02eab50d8d3df41634a854c6dc4a
Couragenwanduka <156565155+Couragenwanduka@users.noreply.github.com>
2024-02-07 14:35:53 +0100

    Update card_validation.js

--- card_validation.js
+++ card_validation.js
@@ -1,61 +1,24 @@
-// Defining a Base class
-class CreditCard{
-  constructor(name,cvv,expireDate,cardNumber){
-    this.name=name;
-    this.cvv=cvv;
-    this.expireDate=expireDate;
-    this.cardNumber=cardNumber;
-    if(this.constructor ===CreditCard){
-      throw new Error('Sorry this class is private')
-    }
-  }
-};
+function cardValidation(cardNumber){
+  // Define regex patterns for each card type
 
-class MasterCard extends CreditCard{
-  constructor(name,cvv,expireDate,cardNumber){
-    super(name,cvv,expireDate,cardNumber);
-    this.isMasterCard=false
-  }
-  cardValidation(cardNumber){
-    // MasterCard numbers start with 5 and are 16 digits long.
-     let number = /^2|5\d{3}\s\d{4}\s\d{4}\s\d{4}/.test(cardNumber)
-  if(number === true){
-    this.isMasterCard=true} 
-  }
-}
-class VisaCard extends CreditCard{
-  constructor(name,cvv,expireDate,cardNumber,bank){
-    super(name,cvv,expireDate,cardNumber);
-    this.bank=bank;
-    this.isVisaCard=false
-  }
-  cardValidation(cardNumber){
-   // VisaCard numbers start with 4 and are 16 digits long
-    let number =/^4\d{3}\s\d{4}\s\d{4}\s\d{4}/.test(cardNumber)
-    if(number === true){
-      this.isVisaCard=true}  
-  }
-};
+ // MasterCard numbers start with 5 or 2and are 16 digits long.
+const masterCardPattern=/^2|5\d{3}\s\d{4}\s\d{4}\s\d{4}/;
+// VisaCard numbers start with 4 and are 16 digits long
+const visaCardPattern=/^4\d{3}\s\d{4}\s\d{4}\s\d{4}/;
+// American Express numbers start with 3 and are 15 digits long
+const americanExpressPattern=/^3\d{3}\s\d{4}\s\d{4}\s\d{3}/;
+ 
 
-class AmericanExpress extends CreditCard{
-  constructor(name,cvv,expireDate,cardNumber,country){
-    super(name,cvv,expireDate,cardNumber)
-    this.country=country;
-    this.isAmericanExpress=false
-  }
-  cardValidation(cardNumber){
-    // American Express numbers start with 3 and are 15 digits long
-    let number =/^3\d{3}\s\d{4}\s\d{4}\s\d{3}/.test(cardNumber)
-    if(number === true){
-      this.isAmericanExpress=true}  
-  }
+// Check if the card number matches any of the patterns
+if(masterCardPattern.test(cardNumber)){
+  return "Master Card";
+}else if(visaCardPattern.test(cardNumber)){
+  return "visa Card"
+}else if (americanExpressPattern.test(cardNumber)){
+  return "American Express"
 }
-const mastercard= new MasterCard("Ogechukwu Ani","233","30/1/2026","5399 4122 6157 4698");
-const visacard= new VisaCard("Mmadu Ugwu","255","26/25/2023","4187 4517 2824 0229","Zenith");
-const americanExpress=new AmericanExpress("obi cubana","444","14/4/2027","3187 4517 2824 029","Nigeria");
-mastercard.cardValidation("5399 4122 6157 4698");
-visacard.cardValidation("4187 4517 2824 0229");
-americanExpress.cardValidation("3187 4517 2824 029");
-console.log(mastercard);
-console.log(visacard);
-console.log(americanExpress);
+ };
+
+ const cardNumber="4187 4517 2824 0229";
+ const cardtype= cardValidation(cardNumber);
+ console.log('The card type is', cardtype)
